83fb5710a3c9b24365b8de2c45963900a22abfa3
Patrick <patrickvyn@gmail.com>
2021-12-06 09:06:32 -0500

    Section 3 Episodes 46-51

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,31 @@
+import React, {useState} from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 //Component = function
 function ExpenseItem(props) {
-    
+
+    const [title, setTitle] = useState(props.title); //Hooks must be in react component functions
+    //useState returns an array where the 1st element is the value and the second is the update value
+    //Always returns two values in that order
+
+
+
+    const clickHandler = () => { //We are not setting the value through an "=" so therefore a Const
+                                 //is valid for title.
+        setTitle('Updated!'); 
+    }
 
     return (
             <Card className = "expense-item">
                 <ExpenseDate date = {props.date}/>
                     <div className = "expense-item__description">
-                        <h2>{props.title}</h2>
+                        <h2>{title}</h2>
                         <div className = "expense-item__price">{props.amount}</div>
                     </div>
+                    <button onClick = {clickHandler}>Change Title</button>
             </Card>
             );
     //<div>Date</div><div><h2>Title</h2></div><div>Amount</div> Doesn't work because you can only have one root snippet per each return
